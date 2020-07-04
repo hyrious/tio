b27f8ac98cdeae768997b6f01a5dff650c9da4ba
hyrious <hyrious@outlook.com>
2020-07-04 13:38:56 +0800

    add icon and [run with args] command, bump version

--- extension.js
+++ extension.js
@@ -4,50 +4,88 @@ const tio = require('./api');
 
 const delay = ms => new Promise(r => setTimeout(r, ms));
 
+let output = null;
+let cachedArgs = "", cachedInput = "", cachedCflags = "";
+
+function invokeTio(code, lang, options) {
+    if (output == null)
+        output = vscode.window.createOutputChannel("tio");
+    output.clear();
+
+    vscode.window.withProgress({
+        location: vscode.ProgressLocation.Notification,
+        title: "Tio: Running"
+    }, async (progress) => {
+        progress.report({ increment: 0 });
+
+        const token = setTimeout(() => {
+            progress.report({ increment: 10, message: "..." });
+        }, 500);
+
+        const { out, err } = await tio.run(code, lang, options);
+
+        clearTimeout(token);
+
+        progress.report({ increment: 90, message: "...done!" });
+
+        if (out) {
+            output.appendLine("Output:");
+            output.appendLine(out);
+            output.show();
+        }
+
+        if (err) {
+            output.appendLine("Info:");
+            output.appendLine(err);
+            output.show();
+        }
+
+        await delay(400);
+    });
+}
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    let disposable = vscode.commands.registerTextEditorCommand('tio.run', async (editor, edit) => {
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand('tio.run', async (editor, _) => {
         const lang = await vscode.window.showQuickPick(tio.languages, { placeHolder: "Language" });
         if (lang == null) return;
+
         const code = editor.document.getText();
-        const output = vscode.window.createOutputChannel("tio");
-        output.clear();
-        vscode.window.withProgress({
-            location: vscode.ProgressLocation.Notification,
-            title: "Tio: Running"
-        }, async (progress) => {
-            progress.report({ increment: 0 });
 
-            const token = setTimeout(() => {
-                progress.report({ increment: 10, message: "..." });
-            }, 500);
+        invokeTio(code, lang, {});
+    }));
 
-            const { out, err } = await tio.run(code, lang);
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand('tio.run2', async (editor, _) => {
+        const lang = await vscode.window.showQuickPick(tio.languages, { placeHolder: "Language" });
+        if (lang == null) return;
 
-            clearTimeout(token);
-            progress.report({ increment: 90, message: "..." })
+        const input = await vscode.window.showInputBox({ value: cachedInput, placeHolder: "Input" });
+        if (input == null) return;
 
-            if (out) {
-                output.appendLine("Output:");
-                output.appendLine(out);
-                output.show();
-            }
+        const args = await vscode.window.showInputBox({ value: cachedArgs, placeHolder: "Args" });
+        if (args == null) return;
 
-            if (err) {
-                output.appendLine("Info:");
-                output.appendLine(err);
-                output.show();
-            }
+        let options = { input, args: args.split(/\s+/) };
+        if (lang.startsWith('c-') || lang.startsWith('cpp-')) {
+            const flags = await vscode.window.showInputBox({ value: cachedCflags, placeHolder: "CFLAGS" });
+            if (flags == null) return;
+            options.flags = flags.split(/\s+/);
+        }
 
-            await delay(400);
-        });
-    });
-    context.subscriptions.push(disposable);
+        const code = editor.document.getText();
+
+        invokeTio(code, lang, options);
+    }));
 }
 
-function deactivate() { }
+function deactivate() {
+    if (output != null) {
+        output.hide();
+        output.dispose();
+    }
+}
 
 module.exports = {
     activate,
